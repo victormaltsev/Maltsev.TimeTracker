c1a0a65f6f233b284273cbb9f67b792fb673e352
victormaltsev <git@victormaltsev.com>
2024-03-17 15:38:15 +0300

    reformat db-01-structure

--- scripts/db-01-structure.sql
+++ scripts/db-01-structure.sql
@@ -5,21 +5,23 @@ CREATE SCHEMA IF NOT EXISTS time_trackers;
 
 --
 
-CREATE TABLE IF NOT EXISTS time_trackers.assignments (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    is_favorite BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS time_trackers.assignments
+(
+    id          UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    name        TEXT        NOT NULL,
+    description TEXT        NOT NULL,
+    is_favorite BOOLEAN     NOT NULL DEFAULT FALSE,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS time_trackers.time_trackers (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    assignment_id UUID NOT NULL,
-    comment TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    started_at TIMESTAMPTZ NOT NULL,
-    finished_at TIMESTAMPTZ NOT NULL,
+CREATE TABLE IF NOT EXISTS time_trackers.time_trackers
+(
+    id            UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    assignment_id UUID        NOT NULL,
+    comment       TEXT        NOT NULL,
+    started_at    TIMESTAMPTZ NOT NULL,
+    finished_at   TIMESTAMPTZ NOT NULL,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
     FOREIGN KEY (assignment_id) REFERENCES time_trackers.assignments (id)
 );
