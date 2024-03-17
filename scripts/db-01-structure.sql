a3b3a69b3ccd9c21798347dab39728f7e549220a
victormaltsev <git@victormaltsev.com>
2024-03-17 07:54:15 +0300

    rename schema

--- scripts/db-01-structure.sql
+++ scripts/db-01-structure.sql
@@ -1,11 +1,11 @@
 --
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE SCHEMA IF NOT EXISTS time_tracker;
+CREATE SCHEMA IF NOT EXISTS time_trackers;
 
 --
 
-CREATE TABLE IF NOT EXISTS time_tracker.assignments (
+CREATE TABLE IF NOT EXISTS time_trackers.assignments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     description TEXT NOT NULL,
