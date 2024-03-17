a90bef8af6dbc328966bd00c37a773cdf64eca2f
victormaltsev <git@victormaltsev.com>
2024-03-17 14:47:10 +0300

    update assignments repository

--- src/Maltsev.TimeTracker.Application/Repositories/IAssignmentsRepository.cs
+++ src/Maltsev.TimeTracker.Application/Repositories/IAssignmentsRepository.cs
@@ -1,31 +1,18 @@
+using Maltsev.TimeTracker.Application.ValueTypes;
+
 namespace Maltsev.TimeTracker.Application.Repositories;
 
 public interface IAssignmentsRepository
 {
+    Task<IEnumerable<Assignment>> GetAssignmentsAsync(AssignmentId assignmentId, CancellationToken cancellationToken = default);
     Task<AssignmentId> InsertAssignmentAsync(Assignment assignment, CancellationToken cancellationToken = default);
     Task UpdateAssignmentAsync(AssignmentId assignmentId, Assignment assignment, CancellationToken cancellationToken = default);
     Task DeleteAssignmentAsync(AssignmentId assignmentId, CancellationToken cancellationToken = default);
-}
-
-public readonly struct AssignmentId(Guid value) : IEquatable<AssignmentId>
-{
-    public Guid Value { get; } = value;
-
-    public bool Equals(AssignmentId other) => Value.Equals(other.Value);
-
-    public override bool Equals(object? obj) => obj is AssignmentId other && Equals(other);
-    public override int GetHashCode() => Value.GetHashCode();
-    public override string ToString() => Value.ToString();
-
-    public static bool operator ==(AssignmentId left, AssignmentId right) => left.Equals(right);
-    public static bool operator !=(AssignmentId left, AssignmentId right) => !(left == right);
 }
 
 public record Assignment
 {
     public required string Name { get; init; }
     public required string Description { get; init; }
-    public required string Comment { get; init; }
     public required bool IsFavorite { get; init; }
-    public required DateTimeOffset CreatedAt { get; init; }
 }
