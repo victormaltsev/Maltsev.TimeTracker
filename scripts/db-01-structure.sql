21d9347ad2c7928ce9934c8d29ec05f140a1a119
victormaltsev <git@victormaltsev.com>
2024-03-17 07:59:46 +0300

    add time_trackers

--- scripts/db-01-structure.sql
+++ scripts/db-01-structure.sql
@@ -13,4 +13,14 @@ CREATE TABLE IF NOT EXISTS time_trackers.assignments (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS time_trackers.time_trackers (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    assignment_id UUID NOT NULL,
+    comment TEXT NOT NULL,
+    started_at TIMESTAMPTZ NOT NULL,
+    finished_at TIMESTAMPTZ NOT NULL,
+
+    FOREIGN KEY (assignment_id) REFERENCES time_trackers.assignments (id)
+);
+
 --
